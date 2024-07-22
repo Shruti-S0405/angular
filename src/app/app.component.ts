c8583477eed8f82c11ce685d59ffa3849b1ea2d0
Shruti <shruti.sundar0405@gmail.com>
2024-07-23 00:20:39 +0530

    synv

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DataService } from './data.service';
 import {CommonModule} from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MemberContactComponent } from './member-contact/member-contact.component';
-import { environment } from '../environments/environment.development';
-
-
-interface User {
-  id: number;   
-  name: string;
-  username: string;
-  email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-  }
-  phone: string;
-  website: string;
-  company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
-  }
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,RouterOutlet, HttpClientModule, MemberContactComponent],
-  providers: [DataService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
-export class AppComponent {
-  // title = 'angular-api-connection';
-  // users: User[] = [];
-
-  // x:string = environment.apiUrl + '/users';
-
-  // constructor(private dataService: DataService){
-  //     this.dataService.get(this.x).subscribe(
-  //       (users : any) => {
-  //         this.users = users as User[];
-  //     },
-  //     () => {}
-  //   );
-  // }
-}
+export class AppComponent {}
--- src/app/member-contact/member-contact.component.ts
+++ src/app/member-contact/member-contact.component.ts
@@ -1,11 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
+import { environment } from '../../environments/environment.development';
+
+interface User {
+  id: number;   
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+  }
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  }
+}
 
 @Component({
   selector: 'app-member-contact',
   standalone: true,
-  imports: [HttpClientModule, CommonModule],
+  imports: [HttpClientModule, CommonModule, ],
+  providers: [DataService],
   templateUrl: './member-contact.component.html',
   styleUrl: './member-contact.component.css'
 })
